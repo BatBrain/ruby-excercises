111d12443d661369ea8b38f88981d6745e28450e
BatBrain <john.ferguson3.141596@gmail.com>
2016-10-11 17:18:33 +0000

    update

---
+++ benchmark_with_block.rb
@@ -0,0 +1,18 @@
+def benchmark(&block)
+  # Your benchmarking code goes here.
+  start_time = Time.now
+
+# Calculate the 200th Fibonacci number
+  block.call
+
+  end_time = Time.now
+end_time - start_time
+end
+
+# Be careful, pasting this into IRB will take a long time to print.
+# It's a loooong string. :)
+long_string = "apple"*100000000
+
+running_time = benchmark { long_string.reverse }
+
+puts "string.reverse took #{running_time} seconds to run"
